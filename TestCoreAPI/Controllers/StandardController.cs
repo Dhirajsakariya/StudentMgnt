bf642a27002b380587cf37aaef89c7adb599b045
janvi67 <117526245+janvi67@users.noreply.github.com>
2024-03-19 18:03:33 +0530

    Update StandardController.cs

--- TestCoreAPI/Controllers/StandardController.cs
+++ TestCoreAPI/Controllers/StandardController.cs
@@ -52,58 +52,6 @@ namespace TestCoreApi.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
-        /*
-        [HttpPut]
-        [Route("PutAdminTeachers{id}")]
-        public async Task<ActionResult> PutAdminTeacher(Guid id, AdminTeacherDto adminTeacherDto)
-        {
-            try
-            {
-                var adminTeacher = await dbContext.AdminTeachers.FindAsync(id);
-
-                if (adminTeacher == null)
-                {
-                    return NotFound();
-                }
-                adminTeacher.Name = adminTeacherDto.Name;
-                adminTeacher.Email = adminTeacherDto.Email;
-                adminTeacher.Password = adminTeacherDto.Password;
-                adminTeacher.Gender = adminTeacherDto.Gender;
-                adminTeacher.BirthDate = adminTeacherDto.BirthDate;
-                adminTeacher.MobileNumber = adminTeacherDto.MobileNumber;
-                adminTeacher.JoinDate = adminTeacherDto.JoinDate;
-                adminTeacher.Address = adminTeacherDto.Address;
-                adminTeacher.City = adminTeacherDto.City;
-                adminTeacher.District = adminTeacherDto.District;
-                adminTeacher.State = adminTeacherDto.State;
-                adminTeacher.PinCode = adminTeacherDto.PinCode;
-                adminTeacher.IsAdmin = adminTeacherDto.IsAdmin;
-
-                dbContext.Entry(adminTeacher).State = EntityState.Modified;
-                await dbContext.SaveChangesAsync();
-                return Ok(adminTeacher);
-
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpDelete]
-        [Route("DeleteAdminTeacher{id:guid}")]
-        public async Task<IActionResult> DeleteAdminTeacher([FromRoute] Guid id)
-        {
-            var adminTeacher = await dbContext.AdminTeachers.FindAsync(id);
-
-            if (adminTeacher != null)
-            {
-                dbContext.Remove(adminTeacher);
-                await dbContext.SaveChangesAsync();
-
-                return Ok();
-            }
-            return NotFound();
-        }*/
+       
     }
 }
