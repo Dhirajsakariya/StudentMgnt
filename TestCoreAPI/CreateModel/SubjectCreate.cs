139ff2a53ca2e24032bef176542a03d57507d805
DaxaChhatrodiya <162785813+DaxaChhatrodiya@users.noreply.github.com>
2024-03-19 17:34:55 +0530

    Subject Controller

---
+++ TestCoreAPI/CreateModel/SubjectCreate.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestCoreApi.CreateModel
+{
+    [NotMapped]
+    public class SubjectCreate
+    {
+        public required string Name { get; set; }
+        public required Guid StandardId { get; set; }
+    }
+}
